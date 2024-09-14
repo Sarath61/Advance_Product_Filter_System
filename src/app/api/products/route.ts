7167a023709330b1834c733eed07de48d8db7a58
Sarath61 <sarathsai4602@gmail.com>
2024-09-14 23:41:44 +0530

    filter basic implementation

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,9 +1,42 @@
 import { db } from "@/db";
-export const POST = async () => {
+import { ProductFilterValidator } from "@/lib/validators/product-validators";
+import { NextResponse } from "next/server";
+
+// class Filter {
+//   private filters: Map<string, string[]> = new Map();
+
+//   hasFilter() {
+//     return this.filters.size > 0;
+//   }
+//   add(key: string, operator: string, value: string | number) {
+//     const filter = this.filters.get(key) || [];
+//     filter.push(
+//       `${key} ${operator} ${typeof value === "number" ? value : `"{value}"`}`
+//     );
+//   }
+//   addRaw(key: string, rawFilter: string) {
+//     this.filters.set(key, [rawFilter]);
+//   }
+//   get() {
+//     const parts: string[] = [];
+//     this.filters.forEach((filter) => {
+//       const groupedValues = filter.join(` OR `);
+//     });
+//   }
+// }
+
+export const POST = async (req: NextResponse) => {
+  const body = await req.json();
+
+  const { color, price, size, sort } = ProductFilterValidator.parse(
+    body.filter
+  );
+
   const products = await db.query({
     topK: 12,
     vector: [0, 0, 0],
     includeMetadata: true,
   });
+
   return new Response(JSON.stringify(products));
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,6 +98,9 @@ export default function Home() {
         {
           filter: {
             sort: filter.sort,
+            color: filter.color,
+            price: filter.price,
+            size: filter.size,
           },
         }
       );
@@ -128,6 +131,8 @@ export default function Home() {
 
   const minPrice = Math.min(filter.price.range[0], filter.price.range[1]);
   const maxPrice = Math.max(filter.price.range[0], filter.price.range[1]);
+
+  console.log(filter);
 
   return (
     <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -333,7 +338,27 @@ export default function Home() {
                         className={cn({
                           "opacity-50": !filter.price.isCustom,
                         })}
-                      ></Slider>
+                        disabled={!filter.price.isCustom}
+                        onValueChange={(range) => {
+                          const [newMin, newMax] = range;
+                          setFilter((prev) => ({
+                            ...prev,
+                            price: {
+                              isCustom: true,
+                              range: [newMin, newMax],
+                            },
+                          }));
+                        }}
+                        value={
+                          filter.price.isCustom
+                            ? filter.price.range
+                            : DEFAULT_CUSTOM_PRICE
+                        }
+                        min={DEFAULT_CUSTOM_PRICE[0]}
+                        defaultValue={DEFAULT_CUSTOM_PRICE}
+                        max={DEFAULT_CUSTOM_PRICE[1]}
+                        step={5}
+                      />
                     </li>
                   </ul>
                 </AccordionContent>
